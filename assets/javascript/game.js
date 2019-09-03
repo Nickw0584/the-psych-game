6678d64d75029df64b4f6a081fba8dd5765658a4
Nick Wilson <nickw0584@icloud.com>
2019-09-03 09:40:18 -0500

    finally working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,67 @@
-const randomLetter =""
-const winsCounter  =  0;// change to lets
-const losesCounter =  0;
-const usersGuesses  =  "";
-const lettersGuessed = [];
-const numberGuesses = 9;
+const randomLetter = ""
+let winsCounter = 0;// change to lets
+let losesCounter = 0;
+let usersGuesses = "";
+let lettersGuessed = [];
+let numberGuesses = 9;
 
 
-const letters = ["a","b", "c", "d", "e", "f", "g", "h",
-"i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", 
-"y", "z"];
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h",
+  "i", "j", "k", "l", "m", "n", "o", "p",
+  "q", "r", "s", "t", "u", "v", "w", "x",
+  "y", "z"];
 
 
 
 function randomLetterChoice() {
-    return computerChoice = letters[Math.floor(Math.random()*letters.length)];
+  return computerChoice = letters[Math.floor(Math.random() * letters.length)];
 }
 
-function resetGame(){
+function resetGame() {
   numberGuesses = 9;
-  Guessed       = [];
+  lettersGuessed = [];
   randomLetterChoice();
 }
 
 randomLetterChoice();
 
-document.onkey = function(event){
- 
+document.onkeyup = function (event) {
+
   const usersGuesses = event.key.toLowerCase();
-   console.log("Computer Letter:" + randomLetter ) ;
-   console.log("User Guesses:" + usersGuesses);
+  console.log("Computer Letter:" + randomLetter);
+  console.log("User Guesses:" + usersGuesses);
 
-}
-if (letters.indexOf(usersGuesses.toLowerCase()) !== -1 ){
 
-if(usersGuesses !== computerChoice){
-  numberGuesses --;
-    console.log("Number Guesses: " + numberGuesses);
-    letterGuessed.push(userGuess);
-    console.log(lettersGuessed);
+  if (letters.indexOf(usersGuesses.toLowerCase()) !== -1) {
+
+    if (usersGuesses !== computerChoice) {
+      numberGuesses--;
+      console.log("Number Guesses: " + numberGuesses);
+      lettersGuessed.push(usersGuesses);
+      console.log(lettersGuessed);
+    }
+    if (usersGuesses === computerChoice) {
+      alert("Correct" + computerChoice + "!")
+      winsCounter++;
+      console.log("wins:" + winsCounter)
+      resetGame();
+    }
+    if (numberGuesses === 0) {
+      alert("wrong" + computerChoice + "play again!");
+      losesCounter++;
+      console.log("Loses:" + losesCounter);
+      resetGame();
+    }
+    update()
+  }
+  else {
+    alert("Press any letter to start the game!");
+  }
 }
-if (userGuess === computerChoice) {
-  alert( "Correct" + computerChoice + "!")
-  wins++;
-  console.log("wins:" + wins)
-  resetGamae();
+
+function update() {
+  document.getElementById("lettersGuessed").textContent = "Guesses Left:" + numberGuesses;
+  document.getElementById("numberGuesses").textContent = "Letters Used:" + lettersGuessed;
+  document.getElementById("winCounter").textContent = "Win:" + winsCounter;
+  document.getElementById("lossesCounter").textContent = "Losses:" + losesCounter;
 }
-if (numberGuesses === 0) {
-  alert("wrong" + computerChoice + "play again!");
-  losses++;
-    console.log("Loses:" + losses);
-    resetGame();
-}
-}
-else {
-  alert("Press any letter to start the game!");
-}
-document.getElementById("number-text").textContent = "Guesses Left:" + numberGuesses;
-document.getElementById("letters-text").textContent = "Letters Used:" + lettersGuessed;
-document.getElementById("win-text").textContent = "Win:" + winsCounter;
-document.getElementById("losses-text").textContent = "Losses:" + losesCounter;
